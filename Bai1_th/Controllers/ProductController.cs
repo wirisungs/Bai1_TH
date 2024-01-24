4f9545cfe5902b9c0f2c9a1bc0b4d6ae034f6f01
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2024-01-24 21:00:10 +0700

    Update Packages & Add CategoryController

---
+++ Bai1_th/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai1_th.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
