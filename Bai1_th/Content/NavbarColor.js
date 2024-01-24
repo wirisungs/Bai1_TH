ce8f724dc4c4916583589efdf3ea1c9a2a34272c
ThrisRH <144113163+ThrisRH@users.noreply.github.com>
2024-01-24 11:03:04 +0700

    Layout

---
+++ Bai1_th/Content/NavbarColor.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var navBarColorChange = document.getElementById("navbarHeader")
+
+    function changeColor() {
+        var colors = ["red", "green", "blue", "orange", "indigo", "violet"];
+        var randomColor = colors[Math.floor(Math.random() * colors.length)];
+        navBarColorChange.style.background = randomColor;
+
+        setTimeout(changeColor, 1000)
+    }
+
+    changeColor();
+});
