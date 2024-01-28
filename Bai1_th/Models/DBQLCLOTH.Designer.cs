b780162f5b00a35e3105c0474a0aa1bbe1b5660d
John Doe <145476276+ductri09072004@users.noreply.github.com>
2024-01-28 20:40:56 +0700

    28/01

--- Bai1_th/Models/DBQLCLOTH.Designer.cs
+++ Bai1_th/Models/DBQLCLOTH.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\trank\Documents\GitHub\Bai1_TH\Bai1_th\Models\DBQLCLOTH.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ductr\Documents\GitHub\Bai1_TH\Bai1_th\Models\DBQLCLOTH.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
